287a9b608c9de2a9ffc79cf27a8b429c1c16099d
Praveen kumar <168343716+Praveenkushinpi@users.noreply.github.com>
2025-05-04 22:33:41 +0530

    Create dataTypes.js

---
+++ JavaScript/dataTypes.js
@@ -0,0 +1,25 @@
+/* primitive data types */
+
+
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("40") - ("3");
+let e = "praveen";
+let f = Symbol(" Hello world ");
+
+
+
+
+
+console.log(a,b,c,d,e,f,)
+
+/* non-primitive data types */
+
+const item = {
+  "Hello": true ,
+  "Shubh": false , 
+  " World": 67,
+  "Newbie": undefined
+}
+console.log(item["World"]) 
